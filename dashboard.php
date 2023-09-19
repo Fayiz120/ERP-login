ed6a699bed855541a72ce0b5bfbacacbdcc7ab33
fayiz120 <mfaisep@gmail.com>
2023-09-19 21:42:27 +0530

    added some styles

--- dashboard.php
+++ dashboard.php
@@ -12,18 +12,25 @@ if (empty($_SESSION["id"])) {
 <html>
 <head>
     <title>Dashboard</title>
+    <link rel="stylesheet" href="dashboard.css">
 </head>
 <body>
-    <h1>Welcome, <?php echo $_SESSION["name"]; ?></h1>
-    <p>Email: <?php echo $_SESSION["email"]; ?></p>
-    <p>Designation: <?php echo $_SESSION["designation"]; ?></p>
-    <p>Date of Joining: <?php echo $_SESSION["date_of_joining"]; ?></p>
-    
-    <!-- Add other dashboard content here -->
-
-
-  
-<a href="logout.php">Logout</a>
-
+    <div class="container">
+        <div class="box">
+            
+            
+            <h1>Welcome, <?php echo $_SESSION["name"]; ?></h1>
+            <p>Email: <?php echo $_SESSION["email"]; ?></p>
+            <p>Designation: <?php echo $_SESSION["designation"]; ?></p>
+            <p>Date of Joining: <?php echo $_SESSION["date_of_joining"]; ?></p>
+            
+            <!-- Add other dashboard content here -->
+        </div>
+        
+        
+        
+        <a href="logout.php">Logout</a>
+        
+    </div>
 </body>
 </html>
